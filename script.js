1e25173865b8f7501ea8c70818f1aa004ef717d9
martincodesenicen <martincodesenicen@gmail.com>
2026-06-01 14:01:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const observer = new IntersectionObserver(entries => {
-
-    entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-
-    });
-
-});
-
-document
-    .querySelectorAll(".hidden")
-    .forEach(el => observer.observe(el));
+console.log("Portfolio listo");
