2d0d176062cd9db9179131f859de91a9f1a7fe57
martincodesenicen <martincodesenicen@gmail.com>
2026-06-30 18:57:05 -0300

    Add image slider functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log("Portfolio listo");
+const sliders = document.querySelectorAll(".slider");
+
+sliders.forEach(slider=>{
+
+    const images = slider.querySelectorAll("img");
+
+    const next = slider.querySelector(".next");
+
+    const prev = slider.querySelector(".prev");
+
+    let current = 0;
+
+    function show(index){
+
+        images.forEach(img=>img.classList.remove("active"));
+
+        images[index].classList.add("active");
+
+    }
+
+    next.addEventListener("click",()=>{
+
+        current++;
+
+        if(current>=images.length)
+            current=0;
+
+        show(current);
+
+    });
+
+    prev.addEventListener("click",()=>{
+
+        current--;
+
+        if(current<0)
+            current=images.length-1;
+
+        show(current);
+
+    });
+
+});
